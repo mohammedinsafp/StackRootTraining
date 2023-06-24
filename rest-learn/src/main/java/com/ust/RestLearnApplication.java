d7710e34f05cd329d82cb92ad4e11db4c5de568b
mohammed insaf p <83545341+mohammedinsafp@users.noreply.github.com>
2023-06-24 17:50:48 +0530

    Delete rest-learn directory

--- rest-learn/src/main/java/com/ust/RestLearnApplication.java
+++
@@ -1,9 +0,0 @@
-package com.ust;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class RestLearnApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RestLearnApplication.class, args);
-	}
-}
